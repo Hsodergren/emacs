f3cf8a36ce798f4ad6a14e8ac98aa9fe1d99c0c7
Henrik Södergren <henrik.sodergren@outlook.com>
2019-05-24 23:36:51 +0200

    Added .gitignore and history file

---
+++ transient/history.el
@@ -0,0 +1,17 @@
+((magit-blame
+  ("-w"))
+ (magit-commit nil)
+ (magit-log
+  (("--" "init.el"))
+  ("--author=Henrik Södergren <henrik.sodergren@outlook.com>"
+   ("--" "init.el")
+   "--graph" "--color" "--decorate")
+  (("--" "init.el")
+   "--graph" "--color" "--decorate")
+  (("--" "init.el")
+   "--graph" "--color" "++header")
+  (("--" "init.el")
+   "--graph")
+  ("-n256" "--decorate"))
+ (magit-push nil)
+ (magit:--author "Henrik Södergren <henrik.sodergren@outlook.com>"))
