b4c4c93a70ecc80422c5397ab39d2e5507e971a8
Henrik Södergren <henrik.sodergren@outlook.com>
2024-09-08 19:02:43 +0200

    clean up init file
    Also remove unused files

--- init.el
+++ init.el
@@ -2,39 +2,14 @@
 (defun emacs-dir (file)
   (concat user-emacs-directory file))
 
-(setq shell-command-switch "-ic")
-(load-file (emacs-dir "funcs.el"))
-
-(add-hook 'after-init-hook (lambda ()
-                             (dark-theme)))
-
 ;; BACKUP FILES
 (setq backup-directory-alist `(("." . ,(emacs-dir "saves"))))
 (setq backup-by-copying t)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(setq use-package-always-ensure 1)
-
-(define-key global-map (kbd "C-c r") (lambda () (interactive) (recompile)))
-
-(setq custom-file (emacs-dir "settings.el"))
-(load custom-file)
-(defun package--save-selected-packages (&rest opt) nil)
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
-(add-hook `prog-mode-hook (lambda ()
-                            (whitespace-cleanup)
-                            (show-paren-mode)))
-
-(add-hook `blackbox-mode-hook (lambda ()
-                (define-key blackbox-mode-map (kbd "j") 'bb-down)
-                (define-key blackbox-mode-map (kbd "k") 'bb-up)
-                (define-key blackbox-mode-map (kbd "h") 'bb-left)
-                (define-key blackbox-mode-map (kbd "l") 'bb-right)
-                (define-key blackbox-mode-map (kbd "r") 'blackbox)))
+(use-package use-package
+  :init
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t))
 
 (use-package json-mode :ensure t)
 
@@ -43,20 +18,28 @@
   :bind (:map lisp-mode-map       ("<return>" . paredit-newline))
   :bind (:map emacs-lisp-mode-map ("<return>" . paredit-newline)))
 
-
-;; themes
 (use-package gruber-darker-theme
+  :load-path "lib/gruber-darker-theme"
   :ensure nil
-  :init
-  (add-to-list 'load-path (emacs-dir "lib/gruber-darker-theme")))
-
-(use-package smart-mode-line
+  :demand t
   :config
-  (sml/setup))
+  (load-theme 'gruber-darker :no-confirm))
 
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(use-package emacs
+  :config
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+  (setq inhibit-startup-screen t)
+  (setq blink-cursor-mode nil)
+  (tool-bar-mode -1)
+  (global-set-key (kbd "C-c e v") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+  (global-set-key (kbd "C-c s v") (lambda () (interactive) (load-file "~/.emacs.d/init.el")))
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+  (add-hook 'before-save-hook 'whitespace-cleanup)
+  (add-hook `prog-mode-hook (lambda ()
+			      (whitespace-cleanup)
+			      (show-paren-mode))))
 
-;; DISABLE MOUSE
 (use-package disable-mouse
   :init
   (global-disable-mouse-mode))
@@ -83,8 +66,7 @@
 
 (use-package expand-region
   :ensure t
-  :config
-  (global-set-key (kbd "C-=") #'er/expand-region))
+  :bind ("C-=" . #'er/expand-region))
 
 (use-package vertico
   :init
@@ -98,13 +80,12 @@
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
+    completion-category-defaults nil
+    completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package embark
   :ensure t
-  :config
-  (global-set-key (kbd "C-.") #'embark-act))
+  :bind ("C-." .#'embark-act))
 
 (use-package embark-consult)
 
@@ -123,8 +104,6 @@
   (setq tab-always-indent t))
 
 ;; bindings
-(global-set-key (kbd "C-c e v") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
-(global-set-key (kbd "C-c s v") (lambda () (interactive) (load-file "~/.emacs.d/init.el")))
 
 (use-package magit
   :bind
@@ -161,13 +140,13 @@
 (use-package eglot :ensure nil
   :config
   (setq-default eglot-workspace-configuration '(:gopls (:hints
-                                                        (:assignVariableTypes t
-                                                         :compositeLiteralFields t
-                                                         :compositeLiteralTypes t
-                                                         :functionTypeParameters t
-                                                         :parameterNames t
-                                                         :rangeVariableTypes t)
-                                                        :usePlaceholders t))))
+			    (:assignVariableTypes t
+			     :compositeLiteralFields t
+			     :compositeLiteralTypes t
+			     :functionTypeParameters t
+			     :parameterNames t
+			     :rangeVariableTypes t)
+			    :usePlaceholders t))))
 
 ;; PYTHON
 (use-package python
@@ -196,19 +175,13 @@
 (use-package tuareg
   :init
   (add-hook 'tuareg-mode (lambda () (progn
-                                      (add-hook 'before-save-hook 'ocp-indent-buffer nil 'local))))
+		      (add-hook 'before-save-hook 'ocp-indent-buffer nil 'local))))
   :hook
   (tuareg-mode . eglot-ensure))
 
-(use-package merlin)
-(use-package dune)
-;; (use-package utop
-;;   :config
-;;   (setq utop-command "opam config exec -- dune utop . -- -emacs"))
-(use-package utop)
-
-;; -- merlin setup ---------------------------------------
-(let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
+(use-package merlin
+  :config
+  (let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
   (when (and opam-share (file-directory-p opam-share))
     ;; Register Merlin
     (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
@@ -223,16 +196,9 @@
     (load-file (concat opam-share "/emacs/site-lisp/ocp-indent.el"))
     (load-file (concat opam-share "/emacs/site-lisp/dune.el"))
     (load-file (concat opam-share "/emacs/site-lisp/dune-flymake.el"))
-    (load-file (concat opam-share "/emacs/site-lisp/dune-watch.el"))))
-
-;; (setq opam-share (substring (shell-command-to-string "opam config var share") 0 -1))
-;; (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-;; ;; So you can do it on a mac, where `C-<up>` and `C-<down>` are used
-;; ;; by spaces.
-;; (define-key merlin-mode-map
-;;   (kbd "C-c <up>") 'merlin-type-enclosing-go-up)
-;; (define-key merlin-mode-map
-;;   (kbd "C-c <down>") 'merlin-type-enclosing-go-down)
+    (load-file (concat opam-share "/emacs/site-lisp/dune-watch.el")))))
+(use-package dune)
+(use-package utop)
 
 ;; DART
 ;; To install dart_language_server run 'pub global activate dart_language_server'
@@ -245,7 +211,6 @@
   :hook
   (dart-mode . eglot-ensure))
 
-(use-package dart-server)
 (use-package flutter)
 (use-package flutter-l10n-flycheck)
 
@@ -267,6 +232,3 @@
 (use-package erlang)
 
 (use-package protobuf-mode)
-
-(define-derived-mode test-mode nil "test")
-(add-to-list 'eglot-server-programs '(test-mode "/home/henrik/programming/ocaml/a_lsp/_build/default/bin/main.exe"))
